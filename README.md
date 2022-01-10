2f7cfd322ed50fc821109ec4ecc395bb66f8170c
mohamedisakr <mohamedisakr@gmail.com>
2022-01-10 09:33:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# My Resume Starter Pack and Digital Portfolio
+
+### <a href="#">LIVE DEMO</a>
+
+##### Original Idea
+
+<a href="https://github.com/tbakerx/react-resume-template/blob/master/README.md">Inspiration
+from Tim Baker (tbakerx)</a>
---
+++ src/Components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Footer = ({data}) => {
+  let networks
+  let website
+
+  if (data) {
+    networks = data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      )
+    })
+    website = data.website
+  }
+
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">{networks}</ul>
+
+          <ul className="copyright">
+            <li>
+              Made by{' '}
+              <a title="Mohamed Sakr" href={website}>
+                Mohamed Sakr
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
